3636484598f71f137f814570ec60f480aa4a8c4e
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2024-10-01 00:04:58 +0300

    add

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the env")
+	}
+
+	fmt.Println("Port:", portString)
 }
